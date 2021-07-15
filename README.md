b5be9c1ccadf5fa1561486a3f6b56fc2e9168a29
Rodrigo Guimarães <rodrigo.silvaguimaraes1992@gmail.com>
2021-07-15 04:22:22 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,14 @@
+# Piano Digital
+
+
+
+## Sobre o piano
+
+Um piano criado com base em HTML, CSS, JS. Cada tecla pressionada é emitido um som.
+
+
+
+ ## Layout do piano
+
+<img src="img-piano.PNG" ></img>
+
---
+++ piano-digital/main.js
@@ -0,0 +1,69 @@
+// get all keys
+const keys = document.querySelectorAll(".key")
+
+    function playNote(event)
+    {       
+        let audioKeyCode = getKeyCode(event);        
+        
+        const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`)
+        
+        // if key exists
+        const cantFoundAnyKey = !key
+        
+        if(cantFoundAnyKey)
+        {
+            return
+        }
+        
+        addPlayingClass(key)
+        playAudio(audioKeyCode)       
+    }
+
+    function addPlayingClass(key)
+    {
+        key.classList.add('playing')
+    }
+
+    function getKeyCode(event)
+    {
+        let keyCode;
+        
+        const iskeyboard = event.type === "keydown"
+        if(iskeyboard)
+        {
+            keyCode = event.keyCode
+        }
+        else 
+        {
+            keyCode = event.target.dataset.key
+        }
+
+        return keyCode
+    }
+
+    function playAudio(audioKeyCode)
+    {
+        const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`)
+        audio.currentTime = 0;
+        audio.play()
+    }
+
+    function removePlayingClass(event)
+    {
+        event.target.classList.remove('playing')
+    }
+
+    function registerEvents()
+    {
+        keys.forEach(function(key){
+            key.addEventListener("click", playNote)
+            key.addEventListener("transitionend", removePlayingClass)
+        }) 
+    
+        //keybord type
+        window.addEventListener("keydown", playNote)
+    }
+
+    window.addEventListener("load", registerEvents)
+
+    
